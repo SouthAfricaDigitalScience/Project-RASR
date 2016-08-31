cdec1fa0e1a17d1b0d7f7ac6aa9f66e4919100fb
Bruce Becker <brucellino@gmail.com>
2016-08-31 11:59:20 +0200

    Intermediate updates

--- ingestion.rb
+++ ingestion.rb
@@ -2,4 +2,18 @@
 require 'rubygems'
 require 'nokogiri'
 
-submission = Nokogiri::XML(open("invenio-upload-template.xml"))
+@submission = Nokogiri::XML(open('invenio-upload-template.xml'))
+@submission.xpath('//datafield').each do |datafield|
+  puts "This is a #{datafield.class} class with #{datafield.count} elements"
+  puts "this submission has #{@submission.xpath('//datafield/subfield').count} subfields"
+
+end
+
+
+puts "loop over children"
+@submission.children.each do |child|
+  puts child.content
+end
+
+
+puts "search for the "
